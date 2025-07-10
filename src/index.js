0ea635294dee40ae22b8fc8ac07d548bd89d0cc1
James Byun <jbyun@atlassian.com>
2025-07-10 19:02:42 +1000

    add app property stuff

--- src/index.js
+++ src/index.js
@@ -59,3 +59,173 @@ exports.runWebTrigger = async (request) => {
     };
   }
 };
+
+exports.runTriggerFunctionAppProperties = async (request) => {
+  try {
+    const reqBody = JSON.parse(request.body);
+    console.log(`Request Body (upsert app prop): ${JSON.stringify(reqBody)}`);
+
+    const propertyKey = reqBody.property_key;
+    const propertyValue = reqBody.property_value;
+
+    let results = [];
+    try {
+      const resAsApp = await asApp().requestConfluence(
+        route`/wiki/api/v2/app/properties/${propertyKey}`,
+        {
+          method: "PUT",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(propertyValue),
+        }
+      );
+
+      const data = await resAsApp.json();
+      results.push({
+        message: `result of storing ${propertyKey} asApp()`,
+        result: data,
+      });
+    } catch (err) {
+      results.push({
+        message: `result of storing ${propertyKey} asApp()`,
+        result: `(error) ${err.message}`,
+      });
+    }
+
+    try {
+      const resAsUser = await asUser().requestConfluence(
+        route`/wiki/api/v2/app/properties/${propertyKey}`,
+        {
+          method: "PUT",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(propertyValue),
+        }
+      );
+
+      const data = await resAsUser.json();
+      results.push({
+        message: `result of storing ${propertyKey} asUser()`,
+        result: data,
+      });
+    } catch (err) {
+      results.push({
+        message: `result of storing ${propertyKey} asUser()`,
+        result: `(error) ${err.message}`,
+      });
+    }
+
+    return {
+      body: JSON.stringify(results),
+      headers: {
+        "Content-Type": ["application/json"],
+        // 'X-Request-Id': [`rnd-${rnd}`]
+      },
+      statusCode: 200,
+      statusText: "Triggered",
+    };
+  } catch (e) {
+    console.error(e);
+    return {
+      body: JSON.stringify({
+        message: `error. ${e}`,
+      }),
+      headers: {
+        "Content-Type": ["application/json"],
+        // 'X-Request-Id': [`rnd-${rnd}`]
+      },
+      statusCode: 409,
+      statusText: "Triggered",
+    };
+  }
+};
+
+exports.runDeleteAppProperties = async (request) => {
+  try {
+    const reqBody = JSON.parse(request.body);
+    console.log(`Request Body (delete app prop): ${JSON.stringify(reqBody)}`);
+
+    const propertyKey = reqBody.property_key;
+
+    let results = [];
+    try {
+      const resAsApp = await asApp().requestConfluence(
+        route`/wiki/api/v2/app/properties/${propertyKey}`,
+        {
+          method: "DELETE",
+          headers: {
+            Accept: "application/json"
+          },
+        }
+      );
+
+      let data = resAsApp.status;
+      if (!resAsApp.ok) {
+        data = await resAsApp.json();
+      }
+      results.push({
+        message: `result of deleting ${propertyKey} asApp()`,
+        result: data,
+      });
+    } catch (err) {
+      results.push({
+        message: `result of deleting ${propertyKey} asApp()`,
+        result: `(error) ${err.message}`,
+      });
+    }
+
+    try {
+      const resAsUser = await asUser().requestConfluence(
+        route`/wiki/api/v2/app/properties/${propertyKey}`,
+        {
+          method: "DELETE",
+          headers: {
+            Accept: "application/json"
+          },
+        }
+      );
+
+      let data = resAsUser.status;
+      if (!resAsUser.ok) {
+        data = await resAsUser.json();
+      }
+
+      results.push({
+        message: `result of deleting ${propertyKey} asUser()`,
+        result: data,
+      });
+    } catch (err) {
+      results.push({
+        message: `result of deleting ${propertyKey} asUser()`,
+        result: `(error) ${err.message}`,
+      });
+    }
+
+    return {
+      body: JSON.stringify(results),
+      headers: {
+        "Content-Type": ["application/json"],
+        // 'X-Request-Id': [`rnd-${rnd}`]
+      },
+      statusCode: 200,
+      statusText: "Triggered",
+    };
+  } catch (e) {
+    console.error(e);
+    return {
+      body: JSON.stringify({
+        message: `error. ${e}`,
+      }),
+      headers: {
+        "Content-Type": ["application/json"],
+        // 'X-Request-Id': [`rnd-${rnd}`]
+      },
+      statusCode: 409,
+      statusText: "Triggered",
+    };
+  }
+};
--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -4,11 +4,11 @@ import api, { route, asApp, asUser } from "@forge/api";
 const resolver = new Resolver();
 
 resolver.define('getText', async (req) => {
-  // console.log(`hello app properties ++++++ :`);
-  // console.log(req);
-  // const res1 = await asApp().requestConfluence(route `/wiki/rest/atlassian-connect/1/addons/my-app-conf/properties`);
-  // const data1 = await res1.json();
-  // console.log(`result: ${JSON.stringify(data1)}`);
+  console.log(`hello app properties ++++++ :`);
+  console.log(req);
+  const res1 = await asApp().requestConfluence(route `/rest/atlassian-connect/1/addons/my-app-conf-glitch/properties`);
+  const data1 = await res1.json();
+  console.log(`result: ${JSON.stringify(data1)}`);
 
   return "KAAA BOOOOM!!! (Forge macro)";
 });
